22951dd55aa10ceaa6a5596836f85548d4465ac9
Kshitij Bhatia <kshitij.bhatia@tvsd.ai>
2024-10-12 01:15:34 +0530

    Initial Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.lightBlueAccent,
+        alignment: Alignment.center,
+        child: const Text("Learning Cubit"),
+      ),
+    );
+  }
+}
