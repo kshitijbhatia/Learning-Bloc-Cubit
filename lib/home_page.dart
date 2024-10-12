a089010f22f1b6d08edfaa1f2ccfe61a8ec133c1
Kshitij Bhatia <kshitij.bhatia@tvsd.ai>
2024-10-12 19:00:29 +0530

    Created simple cubit and bloc for a counter

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,81 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyHomePage extends StatelessWidget {
+class CounterCubit extends Cubit<int> {
+  CounterCubit(super.initialState);
+
+  void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    log("Change in Cubit: ${change.currentState} ${change.nextState}");
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    log("Error: ${error}");
+  }
+}
+
+sealed class CounterEvent {}
+
+class CounterIncrementPressed extends CounterEvent {}
+
+class CounterDecrementPressed extends CounterEvent {}
+
+class CounterState extends Bloc<CounterEvent, int>{
+  CounterState(int initialState) : super(initialState) {
+    on<CounterIncrementPressed>((event, emit) {
+      emit(state + 1);
+    },);
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    log("Change in Bloc: ${change.currentState} ${change.nextState}");
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: IconButton(
+        onPressed: () {
+          if(!context.read<CounterState>().isClosed){
+            context.read<CounterState>().add(CounterIncrementPressed());
+          }
+        },
+        icon: const Icon(Icons.add),
+      ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.lightBlueAccent,
         alignment: Alignment.center,
-        child: const Text("Learning Cubit"),
+        child: BlocConsumer<CounterState, int>(
+          listener: (context, state) {
+            if(state == 5) {
+              context.read<CounterState>().close();
+            }
+          },
+          builder: (context, state) {
+            return Text("$state");
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,8 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:"Learning Bloc Cubit",
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: "Learning Bloc Cubit",
+      home: BlocProvider(
+        create: (context) => CounterState(0),
+        child: MyHomePage(),
+      ),
     );
   }
 }
